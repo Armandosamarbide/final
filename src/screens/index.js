3742c10ba905ad65b9241cd8c65094a989a18d48
Armandosamarbide <martin.salduna@gmail.com>
2023-07-25 03:13:09 -0300

    Trabajo Final

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,6 +1,6 @@
 import Homepage from "./Homepage/Homepage/"
 import DetailPage from "./DetailPage/DetailPage";
-import CartPage from "./CartPage/CartPage";
+import CartPage from "./CartPage/CartPage/"
 import ContactPage from "./ContactPage/ContactPage";
 import { ProductCartCard } from "../components";
 
